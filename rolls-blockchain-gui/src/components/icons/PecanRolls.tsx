d75c58f04c8ac07cd5bb6feeba4058661ef87dcc
technickal1 <nick@henryhousehold.com>
2021-11-06 19:17:00 -0400

    update filenames

--- rolls-blockchain-gui/src/components/icons/PecanRolls.tsx
+++ rolls-blockchain-gui/src/components/icons/PecanRolls.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SvgIcon, SvgIconProps } from '@material-ui/core';
-import { ReactComponent as PecanRollsIcon } from './images/rolls.svg';
+import { ReactComponent as PecanRollsIcon } from './images/hddcoin.svg';
 
 export default function Keys(props: SvgIconProps) {
   return <SvgIcon component={PecanRollsIcon} viewBox="0 0 150 58" {...props} />;
