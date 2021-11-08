d97a057c8a00a20b0f5722d0adeaff63adea92b8
technickal1 <nick@henryhousehold.com>
2021-11-08 01:14:37 -0500

    Update block_rewards.py
    first year of 6 for accelerated growth

--- rolls/consensus/block_rewards.py
+++ rolls/consensus/block_rewards.py
@@ -7,38 +7,39 @@ _blocks_per_year = 1681920  # 32 * 6 * 24 * 365
 
 def calculate_pool_reward(height: uint32) -> uint64:
     """
-    Returns the pool reward at a certain block height. The pool earns 7/8 of the reward in each block. 
-    If the farmer is solo farming, they act as the pool, and therefore earn the entire block reward.
+    Returns the pool reward at a certain block height. The pool earns nothing/nada/zero of the reward
+    in each block. If the farmer is solo farming, they are their own almighty pool, and earn 100% of 
+    the entire block reward.
     
-    These reduction events will not be hit at the exact times (first year, 6 months later, etc), due to 
-    fluctuations in difficulty. They will likely come early, if the network space and VDF rates increase.
+    These reduction events will not be hit at the exact times (first year, etc), due to fluctuations
+    in difficulty. They will probably come early as long as network rates increase.
     """
+    return uint64(int(0))
 
     if height == 0:
-        return uint64(int((7 / 8) * 1000000 * _pecans_per_roll)) # prefarm for future of PecanRolls
-    elif height < 0.5 * _blocks_per_year: 
-        return uint64(int((7 / 8) * 6 * _pecans_per_roll)) # first 6 months - 6 block reward
+        return uint64(int((0 / 8) * 1000000 * _pecans_per_roll))
     elif height < 1 * _blocks_per_year: 
-        return uint64(int((7 / 8) * 3 * _pecans_per_roll)) # first year - 3 block reward
+        return uint64(int((0 / 8) * 6 * _pecans_per_roll))
+    elif height < 2 * _blocks_per_year: 
+        return uint64(int((0 / 8) * 3 * _pecans_per_roll))
     else:
-        return uint64(int((7 / 8) * 1 * _pecans_per_roll)) # 1 block reward indefinitely
+        return uint64(int((0 / 8) * 1 * _pecans_per_roll))
 
 
 def calculate_base_farmer_reward(height: uint32) -> uint64:
     """
     Returns the base farmer reward at a certain block height.
-    The base fee reward is 1/8 of total block reward
+    The base fee reward is 100% of total block reward!
 
-    Returns the coinbase reward at a certain block height. These reduction events will not be
-    hit at the exact times (first year, 6 months later, etc), due to fluctuations in difficulty. 
-    They will likely come early, if the network space and VDF rates increase.
+    These reduction events will not be hit at the exact times (first year, etc), due to fluctuations
+    in difficulty. They will probably come early as long as network rates increase.
     """
 
     if height == 0:
-        return uint64(int((1 / 8) * 1000000 * _pecans_per_roll)) # prefarm for future of PecanRolls
-    elif height < 0.5 * _blocks_per_year: 
-        return uint64(int((1 / 8) * 6 * _pecans_per_roll)) # first 6 months - 6 block reward
+        return uint64(1000000 * _pecans_per_roll) # prefarm for future of PecanRolls
     elif height < 1 * _blocks_per_year: 
-        return uint64(int((1 / 8) * 3 * _pecans_per_roll)) # first year - 3 block reward
+        return uint64(6 * _pecans_per_roll) # first year • 6 block reward
+    elif height < 2 * _blocks_per_year: 
+        return uint64(3 * _pecans_per_roll) # second year • 3 block reward
     else:
-        return uint64(int((1 / 8) * 1 * _pecans_per_roll)) # 1 block reward indefinitely
+        return uint64(1 * _pecans_per_roll) # 1 block reward indefinitely
